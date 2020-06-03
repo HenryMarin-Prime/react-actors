dd30de3ce79a4fd2583abd7b55c53b7c94d20ee0
HenryMarin-Prime <henrymarin@rocketmail.com>
2020-06-02 22:05:44 -0500

    about added

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,14 +1,20 @@
 import React, { Component } from 'react';
 import './App.css';
 import Header from '../Header/Header';
+import About from '../About/About';
+// import Contact from '../Contact/Contact';
+// import Footer from '../Footer/Footer';
 
 class App extends Component {
   render() {
     return (
-      <div className='App'>
+      <div className="App">
         <Header />
+        <About />
+        {/* <Contact /> */}
+        {/* <Footer /> */}
       </div>
-    )
+    );
   }
 }
 
